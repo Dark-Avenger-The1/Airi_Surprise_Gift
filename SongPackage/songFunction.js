af936426716e7c70a9422381afd6d3e40eef9e41
Jasper Sam <jaspersam824@gmail.com>
2025-11-05 20:12:03 +0800

    Almost Finisheddddd. All function of every gift are doneeee!!!!! What left are the background.

--- SongPackage/songFunction.js
+++ SongPackage/songFunction.js
@@ -1,7 +1,18 @@
-let item = document.getElementById(item);
-let phase ={
-    
-}
+let item = document.getElementById("item");
+let song = document.getElementById("song");
+let phase =0;
 item.onclick = ()=>{
+    if(phase==0){
+        item.src="Birthday20SourceImg/Icons/OcarinaAnimation.gif";
+        song.play()
+        phase++;
+    }else{
+        item.src="Birthday20SourceImg/Icons/Ocarina.png";
+        song.pause();
+        phase--;
+    }
 
-}
+    song.addEventListener('ended',()=>{
+        item.src="Birthday20SourceImg/Icons/Ocarina.png";
+    });
+};
